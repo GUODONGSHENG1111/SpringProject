078b601ba31c41d9ce5c47a98113d6624e1234de
sr2qsu <Dongsheng_Guo@SAIC-GM.com>
2020-03-27 14:06:56 +0800

    添加getInfo方法

--- src/main/java/com/Student.java
+++ src/main/java/com/Student.java
@@ -1,5 +1,12 @@
 package com;
 
+import lombok.Data;
+
+@Data
 public class Student extends Person {
     private String school;
+
+    public void getInfo() {
+        System.out.println("小学生");
+    }
 }
